779a0746133255f36ebdd06ae9ba27565021cb44
piyush12346789 <piyushgupta024@gmail.com>
2020-11-02 07:41:04 +0530

    [Piyush] Add: Query to insert data into address book table.

---
+++ SQLQuery3AD.sql
@@ -0,0 +1,4 @@
+insert into AddressBook values
+('Piyush','Gupta','Jahangirpura','Surat','Gujarat',60605,'3889233120', 'qwerty123@gmail.com'),
+('Snehil','Verma','Hostel-H','Raipur', 'Chhattisgarh',98116,'8332443923', 'asdfgh123@gmail.com'),
+('Senendra','Deshlehre','Globe square','Bhilai','Chhattisgarh',10011,'8832322103', 'zxcvbn123@gmail.com')
